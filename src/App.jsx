2abb8cd91ad7fc77c7e9d20f9b1c06316f09e764
unknown <shuvrodevmondal337@gmail.com>
2023-03-30 23:54:09 +0600

    integrated bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
 
   return (
     <div className="App">
-      
+      <h1>Knowledge Blog</h1>
+      <button type="button" className='btn btn-danger'>Base class</button>
     </div>
   )
 }
