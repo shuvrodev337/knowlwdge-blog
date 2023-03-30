b2d0f9f8b788d385f66b78d47ec2ec4cf31ebe9a
unknown <shuvrodevmondal337@gmail.com>
2023-03-31 03:11:13 +0600

    added nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
 
 function App() {
 
 
   return (
     <div className="App">
-      <h1>Knowledge Blog</h1>
-      <button type="button" className='btn btn-danger'>Base class</button>
+      <Header></Header>
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import image from '../../images/user-1.png'
+const Header = () => {
+  return (
+    <div className="container ">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand fw-bold fs-2">Knowledge Blog Everyday</a>
+        <img src={image} alt="writer-image" width="56" height="44" />
+      </div>
+    </nav>
+    <hr />
+    </div>
+  );
+};
+
+export default Header;
